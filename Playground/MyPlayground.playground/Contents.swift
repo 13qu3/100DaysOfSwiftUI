93df5dce01aa3fff932ef7863686b4814a815dcd
leque <leque@posteo.no>
2023-09-09 15:31:23 +0200

    checkpoint 1: basic datatypes

---
+++ Playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//Your goal is to write a Swift playground that:
+
+//Creates a constant holding any temperature in Celsius.
+//Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+//Prints the result for the user, showing both the Celsius and Fahrenheit values.
+
+import Cocoa
+
+let celsius = 1.618
+
+let fahrenheit =   celsius * 9 / 5 + 32
+
+print("Hey look, i converted \(celsius)°C to \(fahrenheit)°F")
+
