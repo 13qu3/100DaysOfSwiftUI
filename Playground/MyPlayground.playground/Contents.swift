4de8d1e67edd3648791e6d85dfe933d4938557fe
leque <leque@posteo.no>
2023-09-10 10:36:57 +0200

    checkpoint 3: loops and conditions

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -12,7 +12,6 @@ enum Size: Int {
 }
 
 func printLineBreak(size: Size) {
-//    print(type(of: size.rawValue))
     print(String(repeating: "-", count: size.rawValue))
 }
 
@@ -24,15 +23,8 @@ func printCheckpoint(checkpoint: Int) {
     printLineBreak(size: Size.no)
 }
 
-// Checkpoint 1:
-// Your goal is to write a Swift playground that:
-
-// Creates a constant holding any temperature in Celsius.
-// Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
-// Prints the result for the user, showing both the Celsius and Fahrenheit values.
 printCheckpoint(checkpoint: 1)
 let celsius = 1.618
-
 let fahrenheit = celsius * 9 / 5 + 32
 
 print("Hey look, i converted \(celsius)°C to \(fahrenheit)°F")
@@ -43,3 +35,22 @@ let uniqueNames = Set(students)
 
 print("There are \(uniqueNames.count) unique names among all students \(students.count)")
 
+printCheckpoint(checkpoint: 3)
+var fizzBuzzOutput = [String]()
+
+for num in 1 ... 100 {
+    if num.isMultiple(of: 3), num.isMultiple(of: 5) {
+        fizzBuzzOutput.append("Fizz Buzz")
+        continue
+    } else if num.isMultiple(of: 3) {
+        fizzBuzzOutput.append("Fizz")
+        continue
+    } else if num.isMultiple(of: 5) {
+        fizzBuzzOutput.append("Buzz")
+        continue
+    } else {
+        fizzBuzzOutput.append(String(num))
+    }
+}
+
+print(fizzBuzzOutput)
