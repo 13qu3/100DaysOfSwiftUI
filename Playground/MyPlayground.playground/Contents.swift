dcba06da9d95a33030369ca369b89751a0ef7f4c
leque <leque@posteo.no>
2023-09-12 22:56:05 +0200

    playing around...with cards 🂡🃑🂱🃁

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -86,3 +86,122 @@ func pythagoras(a: Double, b: Double) -> Double {
 }
 
 pythagoras(a: 3, b: 4)
+
+enum Suit: CaseIterable {
+  case hearts
+  case diamanods
+  case clubs
+  case spades
+}
+
+enum Rank: CaseIterable {
+  case seven
+  case eight
+  case nine
+  case ten
+  case jack
+  case queen
+  case king
+  case ace
+}
+
+struct Card {
+  let suit: Suit
+  let rank: Rank
+}
+
+struct Deck {
+  private let cards: [Card]
+  var drawingPile: [Card]
+  var discardPile: [Card] = []
+  var cardsInGame: [Card] = []
+
+  init() {
+    var cards: [Card] = []
+
+    for suit in Suit.allCases {
+      for rank in Rank.allCases {
+        cards.append(Card(suit: suit, rank: rank))
+      }
+    }
+
+    self.cards = cards
+    self.drawingPile = cards.shuffled()
+  }
+
+  mutating func drawCard() -> Card {
+    if self.drawingPile.isEmpty {
+      self.shuffleDiscardPile()
+    }
+
+    var drawedCard = self.drawingPile.popLast()!
+    self.cardsInGame.append(drawedCard)
+    return drawedCard
+  }
+
+  mutating func discardCard(_ card: Card) {
+    self.discardPile.append(card)
+  }
+
+  mutating func shuffleDiscardPile() {
+    print("discard pile shuffled")
+    self.drawingPile = self.discardPile.shuffled()
+    self.discardPile = []
+  }
+}
+
+struct Game {
+  private var deck: Deck
+
+  init() {
+    self.deck = Deck()
+  }
+
+  mutating func drawCard() {
+    let drawedCard = self.deck.drawCard()
+    print("You drawed a \(drawedCard.rank) of \(drawedCard.suit)!")
+  }
+}
+
+var leGame = Game()
+
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
+leGame.drawCard()
